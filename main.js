3a5cb2b89ead8947c4cd2fd659e9bac52f92cba3
meflems <89581469+meflems@users.noreply.github.com>
2022-03-13 14:07:08 +0700

    update main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+const CrytoJS = require("cryptojs");
+
+var encrypted = CryptoJS.AES.encrypt(anjime, "coinsEnc");
+console.log(encrypted);
+var decrypted = CryptoJS.AES.decrypt(anjime, "coinsEnc");
+console.log(decrypted);
